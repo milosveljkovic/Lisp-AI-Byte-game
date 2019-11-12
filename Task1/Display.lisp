3a3c177a534381b3938855039e1ed07e72bfea26
zbalanovic <z.balanovic@elfak.rs>
2019-11-12 16:18:15 +0100

    fja list-key-assoc

--- Task1/Display.lisp
+++ Task1/Display.lisp
@@ -1,1 +1,10 @@
 (defun DisplayBoard )
+
+
+(defun list-key-assoc(key lista )
+    (if (null lista) '()
+        (if (equalp (caar lista) key) (cadar lista)
+            (list-key-assoc key (cdr lista))
+        )
+    )
+)
