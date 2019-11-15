aa671c354cdc69ec9844156aa1da0abbf0d70b15
Predrag Antic <pedja.antic97@gmail.com>
2019-11-15 21:28:42 +0100

    Zavrsena funkcija za igranje poteza

--- startByte.lisp
+++ startByte.lisp
@@ -4,16 +4,21 @@
     (welcome)
     (readBoardDimension)
     (setq matr (matrixFactoryByte 1 1))
-    ;;(matrixFactoryByte 1 1) ;ovo nam i ne treba bilo je za test ali neka ga
-    ;;(trace playMove)
-    (getValuesFromMove '((C 3) (D 4) 1) matr)
-    (playMove '((C 3) (D 4) 1) matr)
+    
+    ;;dinamicki dodani potezi radi provere funkcije
+    (getValuesFromMove '((C 3) (D 4) 0) matr)
+    (playMove '((C 3) (D 4) 0) matr)
+    (getValuesFromMove '((D 4) (C 5) ) matr)
+    (playMove '((D 4) (C 5) ) matr)
+    (getValuesFromMove '((C 5) (B 4) 2) matr)
+    (playMove '((C 5) (B 4) 2) matr)
+
     (displayBoard)
 )
 
 (defun welcome ()
     (format t "~%. . . . . . . . . . . . . . . . . . . . . . .")
-    (format t "~%. . . . . .     Welcome to BYTE   . . . . . .")
+    (format t "~%. . . . . .    Welcome to BYTE    . . . . . .")
     (format t "~%. . . . . . . . . . . . . . . . . . . . . . .~%")
 )
 
@@ -126,7 +131,7 @@
 (defun getValuesFromMove (move matrix)
         (setq from (cadr (assoc (caar move) letterToNumber)))
         (setq to (cadr (assoc (caadr move) letterToNumber)))
-        (setq elTo (car (reverse (getBitsByKey (list from (1- (cadar move))) matrix))));; Uzima poslednji element iz polja iz kog da saljemo
+        (setq elTo (reverse (getNElementsOfList (reverse (getBitsByKey (list from (1- (cadar move))) matrix)) (caddr move))));; Uzima elemente koje prosledjujemo u potezu
 )
 
 (defun playMove (move matrix)
@@ -146,7 +151,7 @@
                             (list
                                 (caar matrix)
                                 (append 
-                                    (cons elTo '()) ;;spajamo elemenat koji smo uzeli iz polja sa kog saljemo
+                                    elTo ;;spajamo elemenat koji smo uzeli iz polja sa kog saljemo
                                     (cadar matrix) ;;i ostatak liste
                                 )
                             )
@@ -155,7 +160,7 @@
                         (cons
                             (list
                                 (caar matrix)
-                                '()
+                                (getRestOfList elTo (cadar matrix))
                             )
                             (playMove move (cdr matrix))
                         )
@@ -166,11 +171,28 @@
     )
 )
 
+(defun getNElementsOfList (l n) ;; Dobijamo listu elemenata koje hocemo da saljemo na osnovu 3. argumenta iz fje playMove
+    (cond
+        ((null l) '())
+        ((and (not (null n)) (< 0 n)) (getNElementsOfList (cdr l) (1- n)))
+        (t (cons (car l) (getNElementsOfList (cdr l) 0)))
+    )
+)
+
+(defun getRestOfList (nEl l) ;; Dobijamo listu elemenata koji nam ostaju kada posaljemo elemente u drugu listu
+    (cond
+        ((null nEl) l)
+        ((null l) '())
+        ((equalp (car nEl) (car l)) (getRestOfList (cdr nEl) (cdr l)))
+        (t (cons (car l) (getRestOfList (cdr nEl) (cdr l))))
+    )
+)
+
 ;test matrica
 ;;(setq matrix '( ((0 0) (x X o o o) ) ((1 1) (o x o x x o)) ((1 3) (X o o x o x x)) ((1 7) (X)) ((2 0) (O)) ((2 2) (O o o x o x x)) ((2 4) (O)) ((2 6) (O)) ((3 1) (X o o)) ((3 3) (X)) ((3 5) (X)) ((3 7) (X)) ((4 0) (O))     
 ;; ((4 2) (O)) ((4 4) (O o o x x)) ((4 6) (O)) ((5 1) (X o o x x x o)) ((5 3) (X x o x o x o)) ((5 5) (o x x x o)) ((5 7) (X x o x)) ((6 0) (O)) ((6 2) (O)) ((6 4) (O)) ((6 6) (O))))
 
-(startGame)
-
 ;;(cdr (member (list (list to ((1- (cadadr move)))) (getBitsByKey((list to ((1- (cadadr move)))) matrix))) matrix)) spaja dodati deo sa ostatkom liste
 ;;(cdar (member (list (list to (1- (cadadr move))) (getBitsByKey (list to (1- (cadadr move))) matrix)) matrix))
+
+(startGame)
